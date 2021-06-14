120b3ee5fc395d54db5eac514dbc74c56ce232b4
xiang221 <41126704+xiang221@users.noreply.github.com>
2021-06-14 17:12:08 +0800

    登入/註冊/修改的前端
    登入/註冊/修改的前端

--- edit_profile.php
+++ edit_profile.php
@@ -7,11 +7,7 @@ header("Expires: 0");
 if(!isset($_SESSION['account'])){
     die('<meta http-equiv=REFRESH CONTENT=0;url=./login.php>');
 }
-$resultStr = '';
-$resultStr1 = '';
-$resultStr2 = '';
-$resultStr3 = '';
-$resultStr4 = '';
+
 if(isset($_POST['nickname']) && isset($_POST['password'])){
     if($_POST['nickname']!=NULL){
     $sth = $dbh->prepare('SELECT account FROM user WHERE account = ? and pwd = md5(?)');
@@ -21,244 +17,238 @@ if(isset($_POST['nickname']) && isset($_POST['password'])){
             if($_POST['newpwd1'] == $_POST['newpwd2']){
                 $pattern = "/[^A-Za-z0-9]/";
                 //preg_match($pattern, $_POST['newpwd1']); 
-                if(preg_match($pattern, $_POST['newpwd1'])) $resultStr3 ="密碼請勿使用字母、數字以外的字元！";
-                else{
+                if(preg_match($pattern, $_POST['newpwd1'])){
+                    echo "<script type='text/javascript'>";
+                    echo "alert('密碼請勿使用字母、數字以外的字元！');";
+                    echo "</script>"; 
+                }else{
                     $sth2 =  $dbh->prepare('UPDATE user SET nickname = ?, pwd = md5(?) WHERE account = ?');
                     $sth2->execute(array($_POST['nickname'], $_POST['newpwd1'], $_SESSION['account']));
-                    $resultStr = '修改暱稱或密碼成功';
                     $_SESSION['nickname'] = $_POST['nickname'];
+                    echo "<script type='text/javascript'>";
+                    echo "alert('修改暱稱或密碼成功');";
+                    echo "location.href='index.php';";
+                    echo "</script>";
                 }
             }
             else {
-                $resultStr4 = '兩次新密碼填寫不同';
+                echo "<script type='text/javascript'>";
+		        echo "alert('兩次新密碼填寫不同');";
+		        echo "</script>";
             }
         }
         else {
             $sth2 =  $dbh->prepare('UPDATE user SET nickname = ? WHERE account = ?');
             $sth2->execute(array($_POST['nickname'], $_SESSION['account']));
             $_SESSION['nickname'] = $_POST['nickname'];
-            $resultStr = '修改暱稱成功';
+            echo "<script type='text/javascript'>";
+		    echo "alert('修改暱稱成功');";
+		    echo "location.href='index.php';";
+		    echo "</script>";
         }
     }
     else {
-        $resultStr1 = '密碼填寫錯誤';
+        echo "<script type='text/javascript'>";
+		echo "alert('密碼填寫錯誤');";
+		echo "</script>";
     }
 }
 else{
-    $resultStr2 = "暱稱請勿空白";
+    echo "<script type='text/javascript'>";
+    echo "alert('暱稱請勿空白');";
+    echo "</script>";
 }
 }
 ?>
 
-
- 
 <html>
 <head>
-<script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
-    <style>
-        body{
-    margin: 0 0 1em 0;
+<meta name="viewport" content="width=device-width, initial-scale=1">    
+<style>
+      
+body{
+position: relative;
+width: 100%;
+height: 100vh;    
+background-color:#FFFFFF;
+font-family: Noto Sans CJK TC;
+font-size: 16px;    
 }
-.container{
-    font-size: 30px;
-    background-color: #005CAF;
-    margin-top: 0;
-    height: 60px;   
+
+    
+#header{
+background-color:#0D3B66;    
+position: fixed;
+width: 100%;
+height: 16%;
+left: 0px;
+top: 0px;
+z-index:1000;     
 }
-.up-table{
-    padding-top: 13px;
-    margin-right: 30px;
-    width: 100%;            
+    
+    
+h1{
+position:absolute; 
+left: 30%;
+top: 15%;   
+font-style: normal;
+font-weight: normal;
+font-size: 4.5vmin;
+line-height: 100%;
+color: #FFFFFF;  
 }
-.up-table td{
-    font-size: 20px;
+
+p{
+position: absolute;
+left: 14%;
+top: 63%;        
+font-size: 2vmin;
+line-height: 2.2vmin; 
+color: #4B4B4B;    
 }
-.left-table td{
-    padding-left:25px;
-}
-.right-table td{
-    padding-right:25px;
-}
-.up-link{
-    color:white;
-    text-decoration:none;
-}
-.link{
-    color: #113285;
-    font-size: 23px;
-    text-decoration:none;
-}
-.link:hover{
-    text-decoration:underline;
-}
-.body-table{
-    margin-top: 20px;
-    margin-left: 40px;
-}
-.body-table td{
-    padding-top: 10px; 
-}
-#name{
-    padding-right:10px;
-}
-.login{
-    color: #f5e8a2;
-}
+    
 .text{
-    color: #113285;
-    font-size: 20px;
+position: absolute;
+font-size: 2.3vmin;
+line-height: 2.25vmin;    
+color: #000000;        
 }
-.container3{
-    margin-top: 35px ;
+      
+.box{
+position: absolute; 
+width: 45%;
+height: 55%;
+left: 28%;
+top: 30%;
+background: #E1E1E1;
+border: 1px solid #000000;
+box-sizing: border-box;
+border-radius: 10px;       
 }
-font{
-    font-family:Microsoft JhengHei;
-}
-a{
-    font-family:Microsoft JhengHei;
-}
+    
 .submit{
-    position: relative;
-    left:85px; 
+position: absolute;
+width: 13%;  
+left: 43%;
+top: 87%;
+}    
+    
+.result{        
+position: absolute;   
+width: 35%;
+height: 10%;
+left: 58%;
+background: #FFFFFF;
+border: 1px solid #000000;
+box-sizing: border-box;
 }
-.submit1{
-    position: relative;
-    top: 6px;
-    left:197px; 
-    margin-bottom: 10px;
-}
-.result{
-    color:red;
-    position: relative;
-    right: 40px;
-}
-.result1{
-    color:red;
-    font-size: 23px;
-}
-.container4{
-    margin-top: 30px ;
-    margin-left: 30px;
-}
-.container5{
-    margin-top: 30px ;
-    margin-left: 35px;
-    margin-right: 30px;
-}
-.zi_hr_01 {
-    border-width: 0;
-    height: 30px;
-    background-image: url("./line2.png");
-    background-repeat: repeat-x;
-}
-.hr_02{
-    border-width: 0;
-    border-bottom:1px #5f5f5f dashed;
-}
-.title-link{
-    color: #6F3381;
-    font-size: 20px;
-    text-decoration:none;
-    font-weight:bold;
-}
-.title-link:hover{
-    text-decoration:underline;
-}
-.tex{
-    font-size: 14px;
-}
-.text-big{
-    font-size: 27px;
-}
-.container6{
-    margin-top:30px;
-    text-align:left;
-    padding-left:200px;
-}
-.submit3{
-    position: relative;
-    left:210px; 
-}
-.result3{
-    color:red;
-}
-.upp-link{
-            color:#f5e8a2;
-            text-decoration:none;
-        }
 
-    </style>
-<meta http-equiv="pragma" content="no-cache" />       
+.photo{
+position: absolute; 
+width: 40%;
+height: 40%;   
+}
+
+.image-cropper{
+width: 12vmin;
+height: 12vmin;
+position: absolute;
+overflow: hidden;
+background: #FFFFFF;
+border-radius: 50%;
+}
+    
+.image{
+display: inline;
+margin: 0 auto;
+margin-left: -25%; 
+height: 100%;
+width: auto;     
+}
+    
+
+    
+</style>
 </head>
-<body bgcolor="#d4ebfa">
- 
+<body>
+    
+    <header id="header">
+        <a href="index.php"><img src="./photos/images/logo.png" style="position:relative; width: 20%;left: 2.5%;top: 15%;"/></a>
+        <h1>修改</h1>
+        <div class="text" style="color:#FFFFFF;top:25%;left:68%;">    
+        <?php echo $_SESSION['nickname'] ?>   
+        </div>
+        <div class="text" style="color:#FFFFFF;top:55%;left:68%;">    
+        積分:<?php $sth = $dbh->prepare('SELECT point from user where account = ?');
+              $sth->execute(array($_SESSION['account']));
+              $row = $sth->fetch(PDO::FETCH_ASSOC);
+              echo "&nbsp&nbsp&nbsp".$row['point']."</td>"; ?>   
+        </div>
 
-<div class="container" >
-        <table class="up-table" border=0>
-            <tr>
-                <td>
-                    <div align="left">
-                        <table class="left-table" border=0>
-                            <tr>
-                                <?php
-                                    echo "<td><a class=\"up-link\" href=\"./index.php\">返回看板列表</a></td>";
-                                    if(isset($_SESSION['account']) && $_SESSION['account'] != null){
-                                        
-                                        echo "<td class=\"login\"  ><a class=\"upp-link\" href=\"./admin.php\" id=\"name\"><font>Hi, ".$_SESSION['account']." (".htmlspecialchars($_SESSION['nickname']).")</font></a></td>"; 
-                                        echo "<td style=\"color:#f7efc1; \"><i class=\"far fa-smile\"></i>";
-                                        $sth = $dbh->prepare('SELECT point from user where account = ?');
-                                        $sth->execute(array($_SESSION['account']));
-                                        $row = $sth->fetch(PDO::FETCH_ASSOC);
-                                        echo "&nbsp&nbsp&nbsp".$row['point']."</td>";
-                                    } 
-                                ?>
-                            </tr>
-                        </table>
-                    </div>
-                </td>
-                <td>
-                    <div align="right">
-                        <table class="right-table" border=0>
-                            <tr>
-                                <?php
-                                    if(isset($_SESSION['account']) && $_SESSION['account'] != null){ //如果登入
-                                        echo "<td><a class=\"up-link\" href=\"./edit_profile.php\">修改資料</a></td>";
-                                        echo "<td><a class=\"up-link\" href=\"./logout.php\">登出</a></td>";
-                                    }
-                                    else{
-                                        echo "<td><a class=\"up-link\" href=\"./register.php\">註冊</a></td> <td><a class=\"up-link\" href=\"./login.php\">登入</a></td>";
-                                    }
-                                ?>
-                            </tr>
-                        </table>
-                    </div>
-                </td>
-            </tr>
-        </table>
-    </div> 
- 
-<div class="container6"> 
-<?php echo "<font class=\"result1\" >".$resultStr."</font>";?><br>
-<form action="<?php echo basename($_SERVER['PHP_SELF']);?>" method="POST">
-    <img src=" <?php if(isset($_SESSION['account'])){
-    $sth = $dbh->prepare('SELECT path FROM user WHERE account = ?');
-    $sth->execute(array($_SESSION['account']));
-    $row = $sth->fetch(PDO::FETCH_ASSOC);
-    echo $row['path'];
-    }?>" width="20%"><br>
-    <a href="photo.php">修改大頭貼</a><br><br>
-    <font class="text">帳號：</font><?php echo $_SESSION['account'];?><br><br>
-    <font class="text">暱稱：</font><input type="text" name="nickname" value="<?php echo htmlspecialchars($_SESSION['nickname']);?>"><br>
-    <?php echo "<font class=\"result3\">".$resultStr2."</font>";?><br>
-    <font class="text">密碼：</font><input type="password" name="password" placeholder="必填"><br>
-    <?php echo "<font class=\"result3\">".$resultStr1."</font>";?><br>
-    <font class="text">修改密碼：</font><input type="password" name="newpwd1" placeholder="僅修改密碼時需填"><br>
-    <?php echo "<font class=\"result3\">".$resultStr3."</font>";?><br>
-    <font class="text">確認密碼：</font><input type="password" name="newpwd2" placeholder="僅修改密碼時需填"><br>
-    <?php echo "<font class=\"result3\">".$resultStr4."</font>";?><br>
-    <br>
-    <input class="submit3" type="submit">
-</form>
+            
+        <div class="photo" style="top:13%;left:75%;">
+            <div class="image-cropper"><img src="<?php if(isset($_SESSION['account'])){
+                $sth = $dbh->prepare('SELECT path FROM user WHERE account = ?');
+                $sth->execute(array($_SESSION['account']));
+                $row = $sth->fetch(PDO::FETCH_ASSOC);
+                    if($row['path']!=''){
+                        echo $row['path'];
+                    }else{
+                        echo './photos/images/profile.png';}}?>" class="image"/>
+            </div>
+        </div>        
+        <div>    
+        <a href="edit_profile.php"><img src="./photos/images/edit.png" style="position:absolute;width:8%;left:83%;
+top:15%;"/></a>    
+        <a href="logout.php"><img src="./photos/images/logout.png" style="position:absolute;width:8%;left:83%;
+top:55%;"/></a>  
+        </div>     
         
-</div>
-</body></html>
+    </header>
+    
+    <img src="./photos/images/ICON.png" style="position:relative;width:6%;left:47%;
+top:24%;;z-index:100;"/>
+    <center>
+        <div class="box">
+            <div class="text" style="color:#000000;top:15%;left:20%;">    
+            <?php echo $_SESSION['account'] ?>   
+            </div>
+            <div class="photo" style="left: 12%;top: 24%;">
+            <div class="image-cropper" style="width:22vmin;height:22vmin;"><img src="<?php if(isset($_SESSION['account'])){
+                $sth = $dbh->prepare('SELECT path FROM user WHERE account = ?');
+                $sth->execute(array($_SESSION['account']));
+                $row = $sth->fetch(PDO::FETCH_ASSOC);
+                    if($row['path']!=''){
+                        echo $row['path'];
+                    }else{
+                        echo './photos/images/photo.png';}}?>" class="image" /></div></div>
+            <p>建議使用正方形圖片</p>    
+            <a href="photo.php"><img src="./photos/images/edit_photo.png" style="position:absolute; width: 18%;left: 14.5%;top: 73%;"/></a>
+            
+            <form action="<?php echo basename($_SERVER['PHP_SELF']);?>" method="POST">
+                <div>
+                <label for="nickname" class="text" style="top:20%;left:51%; ">暱稱</label>
+                <input type="text" style="top:17%;" class="result" name="nickname">
+                </div>
+                <div>
+                <label for="password" class="text" style="top:37%;left:51%; ">密碼</label>
+                <input type="password" class="result" style="top:34%;" name="password">
+                </div>
+                <div>
+                <label for="password" class="text" style="top:54%;left:46%;">修改密碼</label>
+                <input type="password" class="result" style="top:51%;" name="newpwd1">
+                </div>
+                <div>
+                <label for="password" class="text" style="top:71%;left: 46%;">確認密碼</label>
+                <input type="password" class="result" style="top:68%;" name="newpwd2">
+                </div>
+                <Input Type="Image" class="submit" onclick="submit()" Src="./photos/images/send.png" />
+            </form>
+
+        </div>
+    </center>
+    
+</body>
+</html>
+
+
